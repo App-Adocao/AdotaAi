ddeb80288507fefdf7ad3ebbc8224605af9bbc35
Gustavo Rieg <105006546+gustavorieg@users.noreply.github.com>
2025-03-19 22:58:08 -0300

    models and migrations

---
+++ AdotaAi/database/migrations/2025_03_19_232736_create_clientes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255)->nullable();
+            $table->string('cpf', 14)->nullable();
+            $table->string('numeroendereco', 10)->nullable();
+            $table->foreignId('idcep')->constrained('ceps');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
---
+++ AdotaAi/database/migrations/2025_03_19_234256_create_telefones_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero', 20)->nullable();
+            $table->string('tipotelefone', 50)->nullable();
+            $table->foreignId('idcliente')->constrained('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('telefones');
+    }
+};
